991e4a428b102df08d8d3e9cd7e5a819992cc3fe
Markus Felten <Markus.Felten@gmx.de>
2017-09-12 12:17:10 +0200

    refactor: use - for file names

--- tests/endpoint-test.js
+++ tests/endpoint-test.js
@@ -8,22 +8,22 @@ const chai = require('chai'),
   expect = chai.expect,
   should = chai.should(),
   kti = require('kronos-test-interceptor'),
-  endpoint = require('../dist/Endpoint'),
+  endpoint = require('../dist/endpoint'),
   Interceptor = require('kronos-interceptor').Interceptor;
 
 /* simple owner with name */
 function nameIt(name) {
   return {
     toString() {
-        return name;
-      },
-      get name() {
-        return name;
-      },
-      endpointIdentifier(e) {
-        if (name === undefined) return undefined;
-        return `${this.name}/${e.name}`;
-      }
+      return name;
+    },
+    get name() {
+      return name;
+    },
+    endpointIdentifier(e) {
+      if (name === undefined) return undefined;
+      return `${this.name}/${e.name}`;
+    }
   };
 }
 
@@ -33,23 +33,26 @@ function nameIt(name) {
 function testReceive(name, ep, value, hops, cb) {
   describe(name, () => {
     it(`interceptors ${hops ? hops : 'none'} passed`, done => {
-      ep.receive({
-        value: value
-      }).then(response => {
-        const exp = {
+      ep
+        .receive({
           value: value
-        };
-        if (hops) {
-          exp.hops = hops;
-        }
-        assert.deepEqual(response, exp);
+        })
+        .then(response => {
+          const exp = {
+            value: value
+          };
+          if (hops) {
+            exp.hops = hops;
+          }
+          assert.deepEqual(response, exp);
 
-        if (cb) {
-          cb(done);
-        } else {
-          done();
-        }
-      }).catch(done);
+          if (cb) {
+            cb(done);
+          } else {
+            done();
+          }
+        })
+        .catch(done);
     });
   });
 }
@@ -58,7 +61,8 @@ describe('endpoint', () => {
   describe('base Endpoint', () => {
     const e = new endpoint.Endpoint('e', nameIt('o1'));
     it('no direction', () => assert.isUndefined(e.direction));
-    it('toString', () => assert.equal(e.toString(), 'o1/e(connected=false,open=false)'));
+    it('toString', () =>
+      assert.equal(e.toString(), 'o1/e(connected=false,open=false)'));
     it('identifier', () => assert.equal(e.identifier, 'o1/e'));
     it('isOpen', () => assert.isFalse(e.isOpen));
   });
@@ -69,14 +73,16 @@ describe('endpoint', () => {
       it('isDefault', () => assert.isTrue(se.isDefault));
       it('direction out', () => assert.equal(se.direction, 'out'));
       it('isOpen', () => assert.isFalse(se.isOpen));
-      it('toString', () => assert.equal(se.toString(), 'o1/se(connected=false,open=false)'));
+      it('toString', () =>
+        assert.equal(se.toString(), 'o1/se(connected=false,open=false)'));
     });
     describe('receive', () => {
       const re = new endpoint.ReceiveEndpointDefault('re', nameIt('o1'));
       it('isDefault', () => assert.isTrue(re.isDefault));
       it('direction in', () => assert.equal(re.direction, 'in'));
       it('isOpen', () => assert.isFalse(re.isOpen));
-      it('toString', () => assert.equal(re.toString(), 'o1/re(connected=false,open=false)'));
+      it('toString', () =>
+        assert.equal(re.toString(), 'o1/re(connected=false,open=false)'));
     });
   });
 
@@ -86,28 +92,30 @@ describe('endpoint', () => {
       it('not isDefault', () => assert.isFalse(se.isDefault));
       it('no opposite', () => assert.isUndefined(se.opposite));
       it('not isConnected', () => assert.isFalse(se.isConnected));
-      it('json', () => assert.deepEqual(se.toJSON(), {
-        out: true
-      }));
+      it('json', () =>
+        assert.deepEqual(se.toJSON(), {
+          out: true
+        }));
     });
 
     describe('with hasBeen...', () => {
       let hasBeenConnected, hasBeenDisConnected, oldConnection;
       const se = new endpoint.SendEndpoint('se', nameIt('o1'), {
         hasBeenConnected() {
-            hasBeenConnected = true;
-          },
-          hasBeenDisConnected(endpoint) {
-            oldConnection = endpoint;
-            hasBeenDisConnected = true;
-          }
+          hasBeenConnected = true;
+        },
+        hasBeenDisConnected(endpoint) {
+          oldConnection = endpoint;
+          hasBeenDisConnected = true;
+        }
       });
 
       const re = new endpoint.ReceiveEndpoint('re', nameIt('o2'));
 
       describe('connect', () => {
         se.connected = re;
-        it('hasBeenConnected was called', () => assert.isTrue(hasBeenConnected));
+        it('hasBeenConnected was called', () =>
+          assert.isTrue(hasBeenConnected));
         it('se not isOpen', () => assert.isFalse(se.isOpen));
         it('re not isOpen', () => assert.isFalse(re.isOpen));
         //it('hasBeenDisConnected was not already called', () => assert.isUndefined(hasBeenDisConnected));
@@ -115,7 +123,8 @@ describe('endpoint', () => {
 
       describe('disconnect', () => {
         se.connected = undefined;
-        it('hasBeenDisConnected was called', () => assert.isTrue(hasBeenDisConnected));
+        it('hasBeenDisConnected was called', () =>
+          assert.isTrue(hasBeenDisConnected));
         it('with old connection', () => assert.equal(oldConnection, re));
         it('se not isOpen', () => assert.isFalse(se.isOpen));
         it('re not isOpen', () => assert.isFalse(re.isOpen));
@@ -127,7 +136,8 @@ describe('endpoint', () => {
         const se = new endpoint.SendEndpoint('se', nameIt('o1'));
         it('not isConnected', () => assert.isFalse(se.isConnected));
         it('empty interceptors', () => assert.deepEqual(se.interceptors, []));
-        it('no firstInterceptor', () => assert.isUndefined(se.firstInterceptor));
+        it('no firstInterceptor', () =>
+          assert.isUndefined(se.firstInterceptor));
         it('no lastInterceptor', () => assert.isUndefined(se.lastInterceptor));
       });
 
@@ -144,21 +154,30 @@ describe('endpoint', () => {
           it('otherEnd', () => assert.equal(ep1.otherEnd, ep2));
 
           it('without interceptor', done => {
-            ep1.receive({
-              value: 1
-            }).then(response => {
-              assert.equal(response.value, 1);
-              done();
-            }).catch(done);
+            ep1
+              .receive({
+                value: 1
+              })
+              .then(response => {
+                assert.equal(response.value, 1);
+                done();
+              })
+              .catch(done);
           });
         });
 
-        const ic1 = new kti.TestInterceptor({
-          name: 'ic1'
-        }, ep1);
-        const ic2 = new kti.TestInterceptor({
-          name: 'ic2'
-        }, ep1);
+        const ic1 = new kti.TestInterceptor(
+          {
+            name: 'ic1'
+          },
+          ep1
+        );
+        const ic2 = new kti.TestInterceptor(
+          {
+            name: 'ic2'
+          },
+          ep1
+        );
 
         ep1.interceptors = [ic1, ic2];
 
@@ -168,19 +187,24 @@ describe('endpoint', () => {
           it('ic1->ep2', () => assert.equal(ic2.connected, ep2));
         });
 
-        it('is firstInterceptor', () => assert.equal(ic1, ep1.firstInterceptor));
+        it('is firstInterceptor', () =>
+          assert.equal(ic1, ep1.firstInterceptor));
         it('is lastInterceptor', () => assert.equal(ic2, ep1.lastInterceptor));
 
         describe('json with interceptor', () => {
-          it('toJSON', () => assert.deepEqual(ep1.toJSON(), {
-            out: true,
-            target: 'o2/ep2',
-            interceptors: [{
-              type: 'test-interceptor'
-            }, {
-              type: 'test-interceptor'
-            }]
-          }));
+          it('toJSON', () =>
+            assert.deepEqual(ep1.toJSON(), {
+              out: true,
+              target: 'o2/ep2',
+              interceptors: [
+                {
+                  type: 'test-interceptor'
+                },
+                {
+                  type: 'test-interceptor'
+                }
+              ]
+            }));
         });
 
         testReceive('passes with interceptor', ep1, 2, ['ic1', 'ic2']);
@@ -194,15 +218,21 @@ describe('endpoint', () => {
           const ep2 = new endpoint.ReceiveEndpoint('ep2', nameIt('o1'));
 
           ep1.connected = ep2;
-          const ic1 = new kti.TestInterceptor({
-            name: 'ic1'
-          }, ep1);
+          const ic1 = new kti.TestInterceptor(
+            {
+              name: 'ic1'
+            },
+            ep1
+          );
           ep1.interceptors = [ic1];
 
           ep1.interceptors = [];
-          it('empty interceptors', () => assert.deepEqual(ep1.interceptors, []));
-          it('no firstInterceptor', () => assert.isUndefined(ep1.firstInterceptor));
-          it('no lastInterceptor', () => assert.isUndefined(ep1.lastInterceptor));
+          it('empty interceptors', () =>
+            assert.deepEqual(ep1.interceptors, []));
+          it('no firstInterceptor', () =>
+            assert.isUndefined(ep1.firstInterceptor));
+          it('no lastInterceptor', () =>
+            assert.isUndefined(ep1.lastInterceptor));
 
           describe('connected chain', () => {
             it('ep1->ic1', () => assert.equal(ep1.connected, ep2));
@@ -211,7 +241,7 @@ describe('endpoint', () => {
       });
     });
 
-    describe('interceptors on the receive side', function () {
+    describe('interceptors on the receive side', function() {
       const se = new endpoint.SendEndpoint('se', nameIt('st'));
       const re = new endpoint.ReceiveEndpoint('re', nameIt('rt'));
 
@@ -219,31 +249,52 @@ describe('endpoint', () => {
       re.receive = kti.testResponseHandler;
 
       testReceive('passes without', se, 3, undefined, done => {
-        const ic1 = new kti.TestInterceptor({
-          name: 'ic1'
-        }, re);
-        const ic2 = new kti.TestInterceptor({
-          name: 'ic2'
-        }, re);
+        const ic1 = new kti.TestInterceptor(
+          {
+            name: 'ic1'
+          },
+          re
+        );
+        const ic2 = new kti.TestInterceptor(
+          {
+            name: 'ic2'
+          },
+          re
+        );
 
         re.interceptors = [ic1, ic2];
 
         testReceive('receiving endpoint', se, 4, ['ic1', 'ic2'], done => {
-          const ic3 = new kti.TestInterceptor({
-            name: 'ic3'
-          }, re);
+          const ic3 = new kti.TestInterceptor(
+            {
+              name: 'ic3'
+            },
+            re
+          );
 
           // ep2.receive now at the internalEndpoint
           re.receive = kti.testResponseHandler;
           re.interceptors = [ic3];
 
-          testReceive('receiving endpoint internal endpoint', se, 5, ['ic3'], done => {
-            re.interceptors = undefined;
-            testReceive('receiving endpoint removed interceptos', se, 6, undefined, done => {
+          testReceive(
+            'receiving endpoint internal endpoint',
+            se,
+            5,
+            ['ic3'],
+            done => {
+              re.interceptors = undefined;
+              testReceive(
+                'receiving endpoint removed interceptos',
+                se,
+                6,
+                undefined,
+                done => {
+                  done();
+                }
+              );
               done();
-            });
-            done();
-          });
+            }
+          );
           done();
         });
         done();
@@ -273,7 +324,8 @@ describe('endpoint', () => {
         se.injectNext(in1);
 
         it('still isConnected', () => assert.isTrue(se.isConnected));
-        it('interceptor also isConnected', () => assert.isTrue(in1.isConnected));
+        it('interceptor also isConnected', () =>
+          assert.isTrue(in1.isConnected));
         it('has otherEnd', () => assert.equal(se.otherEnd, re));
 
         describe('remove', () => {
@@ -303,29 +355,32 @@ describe('endpoint', () => {
         createOpposite: true
       });
 
-      beforeEach(function () {
+      beforeEach(function() {
         se.connected = re;
       });
 
       it('sender opposite', () => assert.isTrue(se.opposite.isIn));
-      it('sender opposite opposite', () => assert.equal(se.opposite.opposite, se));
+      it('sender opposite opposite', () =>
+        assert.equal(se.opposite.opposite, se));
 
       it('receiver opposite', () => assert.isTrue(re.opposite.isOut));
-      it('receiver opposite opposite', () => assert.equal(re.opposite.opposite, re));
+      it('receiver opposite opposite', () =>
+        assert.equal(re.opposite.opposite, re));
 
       it('receiver sender', () => assert.equal(re.sender, se));
-      it('sender opposite sender', () => assert.equal(se.opposite.sender, re.opposite));
+      it('sender opposite sender', () =>
+        assert.equal(se.opposite.sender, re.opposite));
 
       describe('open / close with receive', () => {
         describe('after open', () => {
-          before(() => re.receive = request => {});
+          before(() => (re.receive = request => {}));
           it('hasBeenOpened was called', () => assert.isTrue(hasBeenOpened));
           it('se isOpen', () => assert.isTrue(se.isOpen));
           it('re isOpen', () => assert.isTrue(re.isOpen));
         });
 
         describe('after close', () => {
-          before(() => re.receive = undefined);
+          before(() => (re.receive = undefined));
           it('willBeClosed was called', () => assert.isTrue(willBeClosed));
           it('se not isOpen', () => assert.isFalse(se.isOpen));
           it('re not isOpen', () => assert.isFalse(re.isOpen));
@@ -341,14 +396,14 @@ describe('endpoint', () => {
         });
 
         describe('after open', () => {
-          before(() => se.connected = re);
+          before(() => (se.connected = re));
           it('hasBeenOpened', () => assert.isTrue(hasBeenOpened));
           xit('se isOpen', () => assert.isTrue(se.isOpen));
           it('re isOpen', () => assert.isTrue(re.isOpen));
         });
 
         describe('after close', () => {
-          before(() => se.connected = undefined);
+          before(() => (se.connected = undefined));
           it('willBeClosed', () => assert.isTrue(willBeClosed));
           it('se not isOpen', () => assert.isFalse(se.isOpen));
           xit('re not isOpen', () => assert.isFalse(re.isOpen));
